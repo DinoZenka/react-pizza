503b0d51f2b904bd42d1232a427f6f4d0a162ee5
DinoZenka <maxim.puchko123@gmail.com>
2021-07-07 17:47:38 +0300

    v1.6 fixed some UI bugs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Header } from './components'
 import { Home, Basket } from './pages';
 import { Route } from 'react-router-dom';
 import React from 'react';
+import './index.scss';
 
 function App() {
 
   return (
-    <div className="px-4">
-      <div className="max-w-screen-xl px-3 py-4 mx-auto bg-white rounded-lg sm-480:px-8">
+    <div className="content-container">
+      <div className="content">
         <Header />
         <Route exact path='/react_pizza' component={Home} />
         <Route exact path='/basket' component={Basket} />
